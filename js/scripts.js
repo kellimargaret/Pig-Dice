c0b35146cca9194032f8e7e718654f8570e9066a
kelli margaret & Deron Johnson <student@epicodus.com>
2015-09-16 14:13:48 -0700

    Add name form for players.

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,12 @@ Player.prototype.rollDie = function() {
   return dieRoll;
 }
 
+function Game(turn, player1, player2) {
+  this.turn = turn;
+  this.player1 = player1;
+  this.player2 = player2;
+}
+
 $(document).ready(function() {
   var testPlayer = new Player("Sven", 0, 0);
 
@@ -21,6 +27,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     var newRoll = testPlayer.rollDie();
+    if(newRoll === 1) {
+      // $("#roll-player1").hide();
+      // $("#hold-player1").hide();
+    }
 
     $("#turn-score").text(testPlayer.turnScore);
     $("#turn-score").show();
@@ -37,5 +47,24 @@ $(document).ready(function() {
     $("#player-score").text(testPlayer.playerScore);
     $("#turn-score").hide();
     $("#roll-score").hide();
+  });
+
+
+  $("#start-button").click(function() {
+    event.preventDefault();
+
+    var player1Name = $("input#name-player1").val();
+    var player1 = new Player(player1Name, 0, 0);
+
+    var player2Name = $("input#name-player2").val();
+    var player2 = new Player(player2Name, 0, 0);
+
+    var newGame = new Game(1, player1, player2);
+
+    $("#game-form").hide();
+    $("#game-board").show();
+
+    $("#player1-display-name").text(player1Name);
+    $("#player2-display-name").text(player2Name);
   });
 });
