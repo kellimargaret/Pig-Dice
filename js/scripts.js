cf98c5982ffd77b046df1cae38ccf10928ef0838
kelli margaret & Deron Johnson <student@epicodus.com>
2015-09-16 15:01:56 -0700

    Switch players turns

--- js/scripts.js
+++ js/scripts.js
@@ -21,18 +21,57 @@ function Game(turn, player1, player2) {
 }
 
 $(document).ready(function() {
-  var testPlayer = new Player("Sven", 0, 0);
+  var player1;
+  var player2;
+  var newGame;
+
+  $("#start-button").click(function() {
+    event.preventDefault();
+    var player1Name = $("input#name-player1").val();
+    player1 = new Player(player1Name, 0, 0);
+
+    var player2Name = $("input#name-player2").val();
+    player2 = new Player(player2Name, 0, 0);
+
+    newGame = new Game(1, player1, player2);
+
+
+    $("#game-form").hide();
+    $("#game-board").show();
+
+    $("#player1-display-name").text(player1Name);
+    $("#player2-display-name").text(player2Name);
+  });
 
   $(".roll").click(function() {
     event.preventDefault();
+    var currentPlayer;
 
-    var newRoll = testPlayer.rollDie();
-    if(newRoll === 1) {
-      // $("#roll-player1").hide();
-      // $("#hold-player1").hide();
+    if (newGame.turn === 1) {
+      currentPlayer = player1;
+    } else {
+      currentPlayer = player2;
     }
 
-    $("#turn-score").text(testPlayer.turnScore);
+    var newRoll = currentPlayer.rollDie();
+
+    if(newRoll === 1) {
+      if (newGame.turn ===1) {
+        newGame.turn = 2;
+        $("#roll-player1").hide();
+        $("#hold-player1").hide();
+        $("#roll-player2").show();
+        $("#hold-player2").show();
+      } else {
+        newGame.turn = 1;
+        $("#roll-player2").hide();
+        $("#hold-player2").hide();
+        $("#roll-player1").show();
+        $("#hold-player1").show();
+      }
+    }
+
+    $("#turn-score").text(currentPlayer.turnScore);
     $("#turn-score").show();
     $("#roll-score").text(newRoll);
     $("#roll-score").show();
@@ -47,24 +86,5 @@ $(document).ready(function() {
     $("#player-score").text(testPlayer.playerScore);
     $("#turn-score").hide();
     $("#roll-score").hide();
-  });
-
-
-  $("#start-button").click(function() {
-    event.preventDefault();
-
-    var player1Name = $("input#name-player1").val();
-    var player1 = new Player(player1Name, 0, 0);
-
-    var player2Name = $("input#name-player2").val();
-    var player2 = new Player(player2Name, 0, 0);
-
-    var newGame = new Game(1, player1, player2);
-
-    $("#game-form").hide();
-    $("#game-board").show();
-
-    $("#player1-display-name").text(player1Name);
-    $("#player2-display-name").text(player2Name);
   });
 });
