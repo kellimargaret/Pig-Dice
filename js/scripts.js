9eca0295222bffb4e88b162fab9cd395144d3f25
kelli margaret & Deron Johnson <student@epicodus.com>
2015-09-16 11:55:36 -0700

    Add jQuery to get basic funtionality working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
-function Player(playerName, playerScore) {
+function Player(playerName, playerScore, turnScore) {
   this.playerName = playerName;
   this.playerScore = playerScore;
+  this.turnScore = turnScore;
 }
 
 Player.prototype.rollDie = function() {
   var dieRoll = Math.floor(Math.random() * 6) + 1;
   if (dieRoll !== 1) {
-    return this.playerScore += dieRoll;
+    this.turnScore += dieRoll;
+    return dieRoll;
   }
+  return dieRoll;
 }
+
+$(document).ready(function() {
+  var testPlayer = new Player("Sven", 0, 0);
+
+  $("#roll").click(function() {
+    event.preventDefault();
+
+    var newRoll = testPlayer.rollDie();
+
+    $("#turn-score").text(testPlayer.turnScore);
+    $("#roll-score").text(newRoll);
+  });
+});
